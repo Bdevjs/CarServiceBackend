821561260bfec538a4eee733a7bebbee64b60ec7
mehadihassansovon <91381812+mehadihassansovon@users.noreply.github.com>
2022-09-07 22:11:15 +0600

    create api for get all and individual data

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 
 //PORT initiallization
@@ -20,12 +20,20 @@ async function run(){
         const serviceCollection = client.db('geniusCar').collection('service');
 
         //get all data form database
-        app.get('/services',async(req,res)=>{
-            const query ={}
+        app.get('/service',async(req,res)=>{
+            const query ={};
             const cursor = serviceCollection.find(query);
-            const result = cursor.toArray();
+            const result = await cursor.toArray();
             res.send(result);
+            console.log(result)
         
+        });
+
+        app.get(`/service/:id`,async(req,res)=>{
+            const id = req.params.id;
+            const query={_id: ObjectId(id)};
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
         })
         
        
